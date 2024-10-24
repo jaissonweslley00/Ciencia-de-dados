ab5c3c0f5214adb2fe672cb3a066bbcd3ef15f1a
jaissonweslley00 <105284622+jaissonweslley00@users.noreply.github.com>
2024-10-24 08:43:05 -0300

    ...

--- gráficos/informacoes.Globais.js
+++ gráficos/informacoes.Globais.js
@@ -3,13 +3,14 @@ const url = 'https://raw.githubusercontent.com/romulopena/ciencia-de-dados/refs/
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
+    const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
     const pessoasComAcesso = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_dia_estudando)
     const pessoasEstudando = ((pessoasComAcesso / pessoasNoMundo) * 100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média </span> $ {horas} </span> ${dados.tempo_medio} horas e <span>${minutos}</span> minutos conectadas.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
